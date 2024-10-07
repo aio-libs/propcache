8915ac3efca762d75b564fc43d80da36a0aafe5b
J. Nick Koston <nick@koston.org>
2024-10-06 21:57:09 -0500

    Improve typing for under_cached_property decorator

--- src/propcache/_helpers_py.py
+++ src/propcache/_helpers_py.py
@@ -1,9 +1,27 @@
 """Various helper functions."""
 
+import sys
 from functools import cached_property
-from typing import Any, Callable, Dict, Generic, Optional, Protocol, Type, TypeVar
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Generic,
+    Optional,
+    Protocol,
+    Type,
+    TypeVar,
+    Union,
+    overload,
+)
 
 __all__ = ("under_cached_property", "cached_property")
+
+
+if sys.version_info >= (3, 11):
+    from typing import Self
+else:
+    Self = Any
 
 _T = TypeVar("_T")
 
@@ -27,18 +45,27 @@ class under_cached_property(Generic[_T]):
         self.__doc__ = wrapped.__doc__
         self.name = wrapped.__name__
 
-    def __get__(self, inst: _TSelf[_T], owner: Optional[Type[Any]] = None) -> _T:
+    @overload
+    def __get__(  # pragma: no cover
+        self, inst: None, owner: Optional[Type[Any]] = None
+    ) -> Self: ...  # pragma: no cover
+
+    @overload
+    def __get__(  # pragma: no cover
+        self, inst: _TSelf[_T], owner: Optional[Type[Any]] = None
+    ) -> _T: ...  # pragma: no cover
+
+    def __get__(
+        self, inst: Optional[_TSelf[_T]], owner: Optional[Type[Any]] = None
+    ) -> Union[_T, Self]:
+        if inst is None:
+            return self
         try:
-            try:
-                return inst._cache[self.name]
-            except KeyError:
-                val = self.wrapped(inst)
-                inst._cache[self.name] = val
-                return val
-        except AttributeError:
-            if inst is None:
-                return self
-            raise
+            return inst._cache[self.name]
+        except KeyError:
+            val = self.wrapped(inst)
+            inst._cache[self.name] = val
+            return val
 
     def __set__(self, inst: _TSelf[_T], value: _T) -> None:
         raise AttributeError("cached property is read-only")

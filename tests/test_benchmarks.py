ba4ba0799a10d3dfd512451084a903749a21ff5a
J. Nick Koston <nick@koston.org>
2024-10-22 12:45:25 -1000

    Add codspeed benchmarks

---
+++ tests/test_benchmarks.py
@@ -0,0 +1,43 @@
+"""codspeed benchmarks for propcache."""
+
+from pytest_codspeed import BenchmarkFixture  # type: ignore[import-untyped]
+
+from propcache import cached_property, under_cached_property
+
+
+def test_under_cached_property_caching(benchmark: BenchmarkFixture) -> None:
+    """Benchmark for under_cached_property caching."""
+
+    class Test:
+        def __init__(self) -> None:
+            self._cache = {"prop": 42}
+
+        @under_cached_property
+        def prop(self) -> int:
+            return 42
+
+    t = Test()
+
+    @benchmark
+    def _run() -> None:
+        for _ in range(100):
+            t.prop
+
+
+def test_cached_property_caching(benchmark: BenchmarkFixture) -> None:
+    """Benchmark for cached_property caching."""
+
+    class Test:
+        def __init__(self) -> None:
+            self.__dict__["prop"] = 42
+
+        @cached_property
+        def prop(self) -> int:
+            return 42
+
+    t = Test()
+
+    @benchmark
+    def _run() -> None:
+        for _ in range(100):
+            t.prop

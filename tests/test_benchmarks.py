b8c2cfedd6e16571189401da1dce0331dd23127c
Vizonex <VizonexBusiness@gmail.com>
2026-06-13 20:17:47 -0500

    mark as no cover due to being typehint related

--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -4,7 +4,7 @@ import typing as _t
 
 import pytest
 
-if TYPE_CHECKING:
+if _t.TYPE_CHECKING:  # pragma: no cover
     import pytest_codspeed
 else:
     pytest_codspeed = pytest.importorskip("pytest_codspeed")
